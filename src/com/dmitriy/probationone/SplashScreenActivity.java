474f4f9255ce614ba892b9fa9322aed00d9a50b3
Dmitriy9307 <dvornicov.d@gmail.com>
2014-07-18 14:15:46 +0400

    Deleted headlines in activity, set the correct transition between activity

--- src/com/dmitriy/probationone/SplashScreenActivity.java
+++ src/com/dmitriy/probationone/SplashScreenActivity.java
@@ -1,35 +1,45 @@
 package com.dmitriy.probationone;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.Window;
 
 public class SplashScreenActivity extends Activity {
+	
+	Handler h;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_splash_screen);
-
 	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();
-		new Handler(){
+		h = new Handler(){
 			public void handleMessage(android.os.Message msg) {
 				startActivity(new Intent(SplashScreenActivity.this,HomeScreen.class));
 			};
-		}.sendEmptyMessageDelayed(0, 2000);
+		};
+		h.sendEmptyMessageDelayed(0, 2000);
+	}
+	
+	@Override
+	protected void onPause(){
+		super.onPause();
+		if(h.hasMessages(0)){
+			h.removeMessages(0);
+		}
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		finish();
 	}
 
 }
